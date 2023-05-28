9a7fa9917a895f6739fa207ce9f5095b4e528b95
connkat <hi.connkat@gmail.com>
2023-05-28 00:08:12 -0400

    broken button added

--- src/components/AnnoyingModal.tsx
+++ src/components/AnnoyingModal.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import Alert from "react-popup-alert";
+
 import RadioButton from "./RadioButton";
 import "./annoyingModal.css";
 
@@ -10,26 +12,55 @@ type AnnoyingModalProps = {
 };
 
 export default function AnnoyingModal(props: AnnoyingModalProps) {
+	const ages = createAge();
 	const [age, setAge] = useState("");
+	const [alert, setAlert] = useState({
+		text: "This is a alert message",
+		show: false,
+	});
 
 	function handleSubmit(event: any) {
 		setAge(event);
 		props.setHandleModal();
 	}
 
-	const ages = createAge();
+	function onCloseAlert() {
+		setAlert({
+			text: "",
+			show: false,
+		});
+	}
+
+	function onShowAlert() {
+		setAlert({
+			text: "You thought it was that easy? Go find your TRUE age.",
+			show: true,
+		});
+	}
 
 	return (
 		<div className="AnnoyingModal">
 			<div className="modal-overlay" id="modal-overlay"></div>
 			<div className="modal-container">
-				<br /> <br /><br /> <br /><br /> <br /><br /> <br />
 				<h1>To access this site, please confirm your birthday</h1>
 				<h2>This easy to use form should get you started!</h2>
 				<div className="modal-container-controls">
-					<button type="submit" onClick={props.setHandleModal}>
+					<button type="submit" onClick={() => onShowAlert()}>
 						X
 					</button>
+					<Alert
+						header={"Header"}
+						btnText={"Close"}
+						text={alert.text}
+						show={alert.show}
+						onClosePress={onCloseAlert}
+						pressCloseOnOutsideClick={true}
+						showBorderBottom={true}
+						alertStyles={{}}
+						headerStyles={{}}
+						textStyles={{}}
+						buttonStyles={{}}
+					/>
 				</div>
 				<div className="modal-container-content">
 					<div>
