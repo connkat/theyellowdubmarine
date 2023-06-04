e294c4ea56a5bd056dc7479fdb828463a32a2a1f
connkat <hi.connkat@gmail.com>
2023-06-03 23:00:46 -0600

    routing demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,47 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./styles.css";
+// import { useState } from "react";
 
-import { Home, Pogs }from "./pages";
+// import AnnoyingModal from "../components/AnnoyingModal";
+
+import construction from "../assets/consbar.gif";
+import no from "./assets/no.png";
+import RV from "./assets/yellowRV.png";
+
+// import "./home.css";
 
 export default function App() {
+	// const [modal, setModal] = useState(true);
+
+	// function setHandleModal(): void {
+	// 	setModal((prevModal) => !prevModal);
+	// }
 
 	return (
 		<div className="App">
-			<Router>
-				<Routes>
-				<Route path="/" element={<Home />}></Route>
-				<Route path="/pogs" element={<Pogs />}></Route>
-				</Routes>
-			</Router>
+			<div className="wordart">
+				<h1>The Yellow Dubmarine!!!!</h1>
+			</div>
+			<div className="main-body">
+				<div className="subtitle">
+					<h2 className="rainbow">
+						Welcome to the home of Alberta's only yellow mobile stage dedicated
+						to electronic music.
+					</h2>
+				</div>
+				<div className="rv">
+					<img src={RV} alt="RV" style={{ maxWidth: 200 }} />
+				</div>
+
+				<h3 className="warning-text">
+					<img src={no} alt="NO" style={{ maxWidth: 20 }} />
+					WE ARE NOT ASSOCIATED WITH THE{" "}
+					<a target="blank" href="http://freezerburnwrestlingfederation.com">
+						FREEZERBURN WRESTLING FEDERATION
+					</a>
+					<img src={no} alt="NO" style={{ maxWidth: 20 }} />
+				</h3>
+				<img src={construction} alt="Under construction" />
+			</div>
+			{/* {modal && <AnnoyingModal setHandleModal={setHandleModal} />} */}
 		</div>
 	);
 }
