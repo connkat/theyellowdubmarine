20dedc5b39314462cd8168e5f2a7de642d8f9431
connkat <hi.connkat@gmail.com>
2023-06-06 22:47:54 -0600

    added insults

---
+++ src/pogs.ts
@@ -0,0 +1,12 @@
+const pogs: Array<string> = [
+	"Fuck yer burn",
+	"Are you trying to find meaning from a pog?",
+	"Survey says: You could be a little cooler",
+	"You think the Dubmarine is the best stage in the history of Freezerburn",
+	"When Mercury is in Retrograde, you are dumb",
+	"It looks like you will come into a lot of money this year, consider giving it to your good friend Kat",
+	"Why be interesting when you can be...you!",
+	"It is really cute that you think that outfit looks cool",
+];
+
+export { pogs };
