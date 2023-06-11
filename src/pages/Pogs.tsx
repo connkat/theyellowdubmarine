15c20881795af3b73224aa113893fc2f3d05513a
Kat Connolly <hi.connkat@gmail.com>
2023-06-10 23:19:50 -0600

    Update Pogs.tsx

--- src/pages/Pogs.tsx
+++ src/pages/Pogs.tsx
@@ -1,3 +1,4 @@
 export default function Pogs() {
-	return <div className="Pogs">You traded a pog for a thought! Nice work!</div>;
+	return <div className="Pogs">You traded a pog for a thought! Nice work!
+	</div>;
 }
