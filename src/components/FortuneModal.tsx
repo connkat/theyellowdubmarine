280978244bde06d751e5c406e9d7c3e3839f2075
connkat <hi.connkat@gmail.com>
2023-06-14 13:31:30 -0600

    added a bunch of bs

---
+++ src/components/FortuneModal.tsx
@@ -0,0 +1,34 @@
+import RV from "../assets/yellowRV.png";
+
+import "./fortuneModal.css";
+
+type Props = {
+	show: boolean;
+	close: () => void;
+	fortune: string;
+};
+
+export default function FortuneModal(props: Props) {
+	return (
+		<>
+			{props.show ? (
+				<div className="FortuneModal">
+					<div className="modal-header"></div>
+					<button className="close" onClick={() => props.close()}>
+						X
+					</button>
+					<div className="modal-body">
+						<img
+							src={RV}
+							alt="RV"
+							className="modal-rv"
+							style={{ maxWidth: 100 }}
+						/>
+						<h3>Your fortune is:</h3>
+						<p>{props.fortune}</p>
+					</div>
+				</div>
+			) : null}
+		</>
+	);
+}
--- src/pages/Pogs.tsx
+++ src/pages/Pogs.tsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
+
+import FortuneModal from "../components/FortuneModal";
 import { pickNum } from "../helperFunctions";
 import { data } from "../pogs";
-
+import pogsGIF from "../assets/pogs.gif";
 import "./pogs.css";
 
 export default function Pogs() {
-	const fortune = pickNum(8);
+	const [modal, setModal] = useState(false);
+	const Toggle = () => setModal(!modal);
+
+	const fortune = data[pickNum(data.length)];
+
 	return (
 		<div className="Pogs">
-			<h1>You traded a pog for a thought! Nice work!</h1>
-			<h3>Here is your fortune for the day:</h3>
-			{data[fortune]}
+			<div className="pogs-container">
+				<div className="wordart-pogs">
+					<h1>Thank you for visiting the Yellow Dubmarine!</h1>
+				</div>
+				<h2>You traded a pog for a thought! Nice work!</h2>
+				<img src={pogsGIF} className="pogsGIF" alt="pogsGIF" />
+				<h4>Your payment of one (1) pog gets you one (1) fortune:</h4>
+				<button onClick={() => Toggle()}>Click to open your fortune</button>
+				<FortuneModal
+					show={modal}
+					close={() => setModal(false)}
+					fortune={fortune}
+				/>
+			</div>
 		</div>
 	);
 }
