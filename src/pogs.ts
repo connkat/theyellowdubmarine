787f97c4ece9690ccbbb150524db1ee74abafcfe
connkat <hi.connkat@gmail.com>
2023-07-24 14:19:42 -0600

    removed mean fortunes

--- src/pogs.ts
+++ src/pogs.ts
@@ -8,10 +8,6 @@ const data: Array<string> = [
 	"Why be interesting when you can be...you!",
 	"It is really cute that you think that outfit looks cool",
 	"Please feed me more pogs :)",
-	"You have an inventive mind and are inclined to be progressive. You lie a great deal. You make the same mistakes repeatedly because you’re stupid. Everyone thinks you’re a [freaking] jerk.",
-	"You are the pioneer type and think that most people are d**kheads. You are quick tempered, impatient and scornful of advice. You are a prick.",
-	"You are the artistic type and have a difficult time with reality. If you are male, you are nil. Most libra women are wh**es. All libras die of venereal disease.",
-	"You are conservative and afraid of taking risks. You are basically a chicken[crap]. There has never been a capricorn of any importance. You should kill yourself.",
 ];
 
 export { data };
