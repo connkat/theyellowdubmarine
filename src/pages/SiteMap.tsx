13690ec9ca80f6832f9b6c0a40528b2558bf315f
connkat <hi.connkat@gmail.com>
2023-08-25 14:20:14 -0400

    updated site map

--- src/pages/SiteMap.tsx
+++ src/pages/SiteMap.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Layout from "./Layout";
 
 export default function SiteMap() {
@@ -5,6 +6,11 @@ export default function SiteMap() {
 		<div className="SiteMap">
 			<Layout>
 				<h1>Site Map</h1>
+				<div className="site-map-body">
+					<Link to="/">Home</Link>
+					<Link to="/pogs">Pogs</Link>
+					<Link to="/photos">Photos</Link>
+				</div>
 			</Layout>
 		</div>
 	);
