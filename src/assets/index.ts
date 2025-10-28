bd47137c1dc0bb07050d9874e6d51caf7d255670
Kat <hi.connkat@gmail.com>
2025-10-27 19:31:12 -0600

    building images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,11 +3,11 @@ import coral from './backgrounds/coral.jpg';
 import pogsBg from './backgrounds/pogs-background.jpg';
 import water2 from './backgrounds/water2.png';
 //clipArt
-// import babyGif from './clipArt/baby.gif';
-import consbarGif from './clipArt/consbar.gif';
-import consbarPng from './clipArt/consbar.png';
-import no from './clipArt/no.png';
-import pogsGIF from './clipArt/pogs.gif';
+// import babyGif from './clipart/baby.gif';
+import consbarGif from './clipart/consbar.gif';
+import consbarPng from './clipart/consbar.png';
+import no from './clipart/no.png';
+import pogsGIF from './clipart/pogs.gif';
 //other
 import yellowRV from './yellowRV.png';
 
