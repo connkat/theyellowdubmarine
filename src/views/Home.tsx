d0fac62dabe2629a2b7e0aabbb4505a1fd41ded3
Kat <hi.connkat@gmail.com>
2026-03-25 20:01:30 -0600

    mobile

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -10,24 +10,24 @@ export default function Home() {
       className="min-h-full flex flex-col bg-repeat bg-[length:20%] bg-[url('/backgrounds/water2.png')]"
     >
       <div className="wordart">
-        <h1>The Yellow Dubmarine!!!!</h1>
+        The Yellow<br className="md:hidden" /> Dubmarine!!!!
       </div>
       <Card>
-        <Image src={yellowRV} alt="RV" width={200} className="rv pt-[5vh]" />
+        <Image src={yellowRV} alt="RV" width={0} height={0} sizes="100vw" className="rv pt-[5vh] w-[100px] h-auto" />
         <div>
           <h3 className="rainbow text-2xl [font-family:'Comic_Sans_MS',cursive]">
             Welcome to the home of Alberta's only mobile yellow stage dedicated to electronic music.
           </h3>
         </div>
         <div className="pb-4 flex flex-col items-center">
-          <p className="warning-text pt-[2vh] text-sm md:text-base">
+          <div className="warning-text pt-[2vh] text-sm md:text-base">
             <Image src={no} alt="NO" width={16} height={16} className="inline" />
             WE ARE NOT ASSOCIATED WITH THE{' '}
             <a target="blank" href="https://freezerburn-wrestling-federation.netlify.app">
               FREEZERBURN WRESTLING FEDERATION
             </a>
             <Image src={no} alt="NO" width={16} height={16} className="inline" />
-          </p>
+          </div>
         </div>
       </Card>
     </div>
