6977ee6e0b89647b3289cabdcde0dabcf2c55d4f
Kat <hi.connkat@gmail.com>
2026-03-25 19:42:31 -0600

    build

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -3,6 +3,6 @@ import { createClient } from '@sanity/client'
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: 'production',
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION ?? '2025-12-24',
+  apiVersion: '2025-12-24',
   useCdn: true,
 })
